4d9fdd4de38bc7f26a9df7e33d9ed42b08541115
ajmengistu <a_mengistu@outlook.com>
2019-06-07 18:24:24 -0400

    Added User model

---
+++ src/main/java/com/bookstore/onlinebookstore/model/User.java
@@ -0,0 +1,83 @@
+package com.bookstore.onlinebookstore.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@Column(name = "user_id", nullable = false, updatable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false, updatable = false, length = 50)
+	private String firstName;
+	@Column(nullable = false, updatable = false, length = 50)
+	private String lastName;
+	@Column(nullable = false, updatable = true, length = 50, unique = true)
+	private String email;
+	@Column(nullable = false, updatable = true, length = 255)
+	private String password;
+	@Column(nullable = false, updatable = false)
+	private Date dateCreated;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", password=" + password + "]";
+	}
+}
