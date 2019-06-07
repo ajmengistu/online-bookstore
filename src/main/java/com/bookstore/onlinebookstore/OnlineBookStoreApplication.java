287fd56cae2bc813d1eefb731f091a3186a64670
ajmengistu <a_mengistu@outlook.com>
2019-06-07 17:30:20 -0400

    Renamed app, added HomeController, & added home.jsp page

--- src/main/java/com/bookstore/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/bookstore/onlinebookstore/OnlineBookStoreApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AthenaAirlines1Application {
+public class OnlineBookStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AthenaAirlines1Application.class, args);
+		SpringApplication.run(OnlineBookStoreApplication.class, args);
 	}
-
 }
---
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Home - OnlineBookStore</title>
+	<link rel="stylesheet" type="text/css" href="webjars/bootstrap/4.3.1/css/bootstrap.min.css" />
+</head>
+<body>
+<div class="container">
+	<h1>Home page!</h1>
+	</div>
+</body>
+</html>
