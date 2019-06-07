2e379c9a05b10193dc95d9399ed67720f3172af8
ajmengistu <a_mengistu@outlook.com>
2019-06-07 15:21:25 -0400

    Initial Spring Boot project commit

---
+++ src/main/java/com/bookstore/onlinebookstore/AthenaAirlines1Application.java
@@ -0,0 +1,13 @@
+package com.bookstore.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaAirlines1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenaAirlines1Application.class, args);
+	}
+
+}
