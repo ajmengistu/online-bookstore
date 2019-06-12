b9ec5e8fede82588959a54c666320a1dbe080ee5
ajmengistu <a_mengistu@outlook.com>
2019-06-11 20:10:51 -0400

    Added BookRepository::findTop25ByOrderByRatingsDesc() method

--- src/main/java/com/bookstore/onlinebookstore/repository/BookRepository.java
+++ src/main/java/com/bookstore/onlinebookstore/repository/BookRepository.java
@@ -1,8 +1,13 @@
 package com.bookstore.onlinebookstore.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bookstore.onlinebookstore.model.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+	public List<Book> findTop25ByOrderByRatingsDesc();
 }
