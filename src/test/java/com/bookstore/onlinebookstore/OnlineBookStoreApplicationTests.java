b64cbead668d109a6ad44ddeca7b08c3b1f8acae
ajmengistu <a_mengistu@outlook.com>
2019-06-20 21:06:05 -0400

    Modified src/test/java class name

--- src/test/java/com/bookstore/onlinebookstore/OnlineBookStoreApplicationTests.java
+++ src/test/java/com/bookstore/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AthenaAirlines1ApplicationTests {
+public class OnlineBookStoreApplicationTests {
 
 	@Test
 	public void contextLoads() {
