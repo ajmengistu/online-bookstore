6b0525289acda8f10c35a53be4a973d46449b413
ajmengistu <a_mengistu@outlook.com>
2019-11-25 18:23:21 -0500

    Switched app MySQL database to a Docker container from a Windows MySQL server. Also, added books.sql file to initialize  the database with books data.

--- src/main/java/com/bookstore/onlinebookstore/repository/BookRepository.java
+++ src/main/java/com/bookstore/onlinebookstore/repository/BookRepository.java
@@ -18,7 +18,10 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
 	public List<Book> findTop25ByOrderByAverageRatingDesc();
 
-	@Query(value = "SELECT * from books group by pub_year having max(average_rating) order by pub_year desc limit 25", nativeQuery = true)
+	@Query(value = "WITH temp AS (SELECT b.book_id, b.authors, b.title, b.average_rating, b.book_format, b.image, b.isbn, b.price, b.pub_year, b.ratings, b.stock"
+			+ " FROM (SELECT pub_year, MAX(average_rating) AS average_rating FROM books GROUP BY pub_year ORDER BY pub_year DESC LIMIT 25) AS x"
+			+ " INNER JOIN books AS b ON x.pub_year=b.pub_year AND x.average_rating=b.average_rating)"
+			+ " SELECT * FROM temp ORDER BY pub_year DESC;", nativeQuery = true)
 	public List<Book> findTop25RatedBooksByYearDesc();
 
 	public List<Book> findTop25ByOrderByPubYearAsc();
